45972eedbcad8e704d474366a2952bc7f9723608
Allyssa Bautista <allyssabautista025@gmail.com>
2025-05-23 21:36:00 +0800

    ..

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once 'config.php';
+require_once 'public/config.php';
 
 if (!isset($_SESSION['user_id'])) {
     header("Location: login-page.php");
--- public/server.js
+++ public/server.js
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(bodyParser.json());
 
 const db = mysql.createConnection({
-    host: 'localhost',
+    host: 'www.i-bulong.com',
     user: 'elective',
     password: 'elective-project', 
     database: 'chat'
