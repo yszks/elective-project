5b30ecdc8a4d14bac8fdef4a092b1eebdbf24465
Allyssa Bautista <allyssabautista025@gmail.com>
2025-05-24 03:01:33 +0800

    asa

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 // Enable CORS for your frontend domain
 app.use(cors({
-  origin: 'https://www.i-bulong.com',
+  origin: '*',
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type']
 }));
