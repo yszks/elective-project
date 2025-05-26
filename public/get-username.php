b02f9711ae32280c4675891aafb02c934f7fbc4a
Carlos Vincent Alejo <carlos.alejo0603@gmail.com>
2025-05-27 01:46:22 +0800

    Update get-username.php

--- public/get-username.php
+++ public/get-username.php
@@ -1,14 +1,5 @@
 <?php
 session_start();
 header('Content-Type: application/json');
-
-$appId = getenv('AGORA_APP_ID');
-$appCertificate = getenv('AGORA_APP_CERTIFICATE');
-
-if (isset($_SESSION['username'])) {
-    echo json_encode(['username' => $_SESSION['username']]);
-} else {
-    http_response_code(401); // Unauthorized
-    echo json_encode(['error' => 'User not logged in']);
-}
+echo json_encode($_SESSION);
 ?>
