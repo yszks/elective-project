fbbe327d12e5d43ae7badde379191c32550fa6b1
Carlos Vincent Alejo <carlos.alejo0603@gmail.com>
2025-05-27 01:58:39 +0800

    Update get-username.php

--- public/get-username.php
+++ public/get-username.php
@@ -1,5 +1,16 @@
 <?php
+header('Access-Control-Allow-Origin: https://www.i-bulong.com');
+header('Access-Control-Allow-Credentials: true');
 session_start();
 header('Content-Type: application/json');
-echo json_encode($_SESSION);
+
+$appId = getenv('AGORA_APP_ID');
+$appCertificate = getenv('AGORA_APP_CERTIFICATE');
+
+if (isset($_SESSION['username'])) {
+    echo json_encode(['username' => $_SESSION['username']]);
+} else {
+    http_response_code(401); // Unauthorized
+    echo json_encode(['error' => 'User not logged in']);
+}
 ?>
